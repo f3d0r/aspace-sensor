f3f628916e91ff1f10cf2ce5a295bff1fd8099d8
Fedor Paretsky <me@f3d0r.com>
2018-08-03 12:53:38 -0700

    Update networking test code with sample call to aspace API

--- misc_mp_code/test.py
+++ misc_mp_code/test.py
@@ -22,25 +22,6 @@ def setupNetwork():
             utime.sleep(1)
     return nic.ifconfig()
 
-def httpGetTest():
-    s = _socket.socket()
-
-    ai = _socket.getaddrinfo("api.trya.space", 443)
-    # print("Address infos:", ai)
-    addr = ai[0][-1]
-
-    # print("Connect address:", addr)
-    s.connect(addr)
-
-    s = ssl.wrap_socket(s)
-    print(s)
-
-    s.write(b"GET /v1 HTTP/1.0\r\n\r\n")
-    res = s.read(4096)
-    jsonRes = ujson.loads(res)
-    print(jsonRes)
-    s.close()
-
 def requestTest():
     response = urequests.get('https://api.trya.space/v1')
     return response.json()
@@ -49,18 +30,19 @@ def updateSensorStatus(spotID, newOccupancyStatus, authKey = ASPACE_AUTH_KEY):
     response = urequests.post('https://api.trya.space/v1/parking/update_status?spot_id=' + str(spotID) + '&occupied=' + str(newOccupancyStatus) + '&auth_key=' + str(authKey))
     return response.json()
 
+def updateSpotStatusTest():
+    updateSpotResponse = updateSensorStatus(841, 'T')
+    utime.sleep(2)
+    updateSpotResponse = updateSensorStatus(841, 'N')
+
 # TESTING DEVICE
-# print("NETWORK CONFIG            :", setupNetwork())
+print("NETWORK CONFIG            :", setupNetwork())
 setupNetwork()
-# print("MAC ADDRESS               :", ubinascii.hexlify(network.WLAN().config('mac'),':').decode())
-# print("\nWIFI TEST:")
+print("MAC ADDRESS               :", ubinascii.hexlify(network.WLAN().config('mac'),':').decode())
 responseData = requestTest()
-print("RESPONSE_INFO:",responseData['res_info']['code_info'])
-print("RESPONSE_CODE:",responseData['res_info']['code'])
-updateSpotResponse = updateSensorStatus(841, 'T')
-utime.sleep(5)
-updateSpotResponse = updateSensorStatus(841, 'F')
-utime.sleep(5)
-updateSpotResponse = updateSensorStatus(841, 'N')
-# httpGetTest()
+
+if (responseData['res_info']['code'] == 30):
+    updateSpotStatusTest()
+else:
+    print("SERVER DIDN'T RESPOND NORMALLY, EXITING...")
 # aspacefun.continueWithDataCapture(i2cDev)
